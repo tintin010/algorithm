325f2da1576bb572cf412b5753b0578163932c9e
tintin010 <82323029+tintin010@users.noreply.github.com>
2023-07-07 16:19:23 +0900

    [Gold V] Title: 내려가기, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -1,49 +1,48 @@
+// 2096 내려가기
+
 #include <iostream>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
+int n, mini, maxi;
+int arr[3], maxx[3], minn[3];
+int t1, t2;
 
-int mn(int a, int b) {
-    if (a > b)
-        return b;
-    return a;
+int mn(int a, int b) { // 최소 값 리턴
+	if (a > b)
+		return b;
+	return a;
 }
-
-int mx(int a, int b) {
-    if (a < b)
-        return b;
-    return a;
+int mx(int a, int b) { // 최대 값 리턴
+	if (a < b)
+		return b;
+	return a;
 }
 
 int main() {
-    int n;
-    cin >> n;
+	cin >> n;
+	cin >> maxx[0] >> maxx[1] >> maxx[2];
+	minn[0] = maxx[0];
+	minn[1] = maxx[1];
+	minn[2] = maxx[2];
 
-    int maxx[3], minn[3];
-    cin >> maxx[0] >> maxx[1] >> maxx[2];
-    minn[0] = maxx[0];
-    minn[1] = maxx[1];
-    minn[2] = maxx[2];
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> arr[j];
+		}
+		t1 = maxx[0]; t2 = maxx[2];
+		maxx[0] = mx(maxx[0], maxx[1]) + arr[0];
+		maxx[2] = mx(maxx[2], maxx[1]) + arr[2];
+		maxx[1] = mx(mx(t1, maxx[1]), t2) + arr[1];
 
-    int arr[3];
-    int t1, t2;
+		t1 = minn[0]; t2 = minn[2];
+		minn[0] = mn(minn[0], minn[1]) + arr[0];
+		minn[2] = mn(minn[2], minn[1]) + arr[2];
+		minn[1] = mn(mn(t1, minn[1]), t2) + arr[1];
 
-    for (int i = 1; i < n; i++) {
-        cin >> arr[0] >> arr[1] >> arr[2];
-        t1 = maxx[0]; t2 = maxx[2];
-        maxx[0] = mx(maxx[0], maxx[1]) + arr[0];
-        maxx[2] = mx(maxx[2], maxx[1]) + arr[2];
-        maxx[1] = mx(mx(t1, maxx[1]), t2) + arr[1];
-
-        t1 = minn[0]; t2 = minn[2];
-        minn[0] = mn(minn[0], minn[1]) + arr[0];
-        minn[2] = mn(minn[2], minn[1]) + arr[2];
-        minn[1] = mn(mn(t1, minn[1]), t2) + arr[1];
-    }
-
-    int maxi = mx(mx(maxx[0], maxx[1]), maxx[2]);
-    int mini = mn(mn(minn[0], minn[1]), minn[2]);
+	}
+    
+    maxi = mx(mx(maxx[0], maxx[1]), maxx[2]);
+	mini = mn(mn(minn[0], minn[1]), minn[2]);
+    
     cout << maxi << " " << mini << endl;
-    return 0;
 }
