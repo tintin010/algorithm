ac534543adfc5eac7c848bf5f0c0a0d8a8073e54
tintin010 <82323029+tintin010@users.noreply.github.com>
2023-08-04 11:39:52 +0900

    [Gold II] Title: 트리의 지름, Time: 312 ms, Memory: 10872 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,50 @@
+// 1167 트리의 지름
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector <pair<int, int>>v[100001];
+int n, anss, temp;
+int vis[100001];
+
+void dfs(int a, int len) {
+	vis[a]++;
+
+	if (anss < len) {
+		temp = a;
+		anss = len;
+	}
+
+	for (int i = 0;i < v[a].size();i++) {
+		int n = v[a][i].first;
+		int dis = v[a][i].second;
+		if (vis[n])continue;
+		vis[n]++;
+		dfs(n, len + dis);
+		vis[n]--;
+	}
+}
+
+int main() {
+	cin >> n;
+	for (int i = 1;i < n;i++) {
+		int st, ed, dis;
+		cin >> st;
+		while (true) {
+			cin >> ed;
+			if (ed == -1)break;
+			cin >> dis;	
+			v[st].push_back({ ed, dis });
+			v[ed].push_back({ st,dis });
+		}
+	}
+	dfs(1, 0);
+	anss = 0;
+	for (int i = 1;i <= n;i++)vis[i] = 0;
+	dfs(temp, 0);
+
+	cout << anss;
+}
