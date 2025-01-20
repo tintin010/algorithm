007c7bca799549b6bcc8d9a1d6276344d46b6a8d
tintin010 <82323029+tintin010@users.noreply.github.com>
2025-01-20 16:57:25 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -2,44 +2,25 @@
 #include <string>
 
 using namespace std;
-
-int n, m;
+int n, m, cntt;
 string s;
-int answer = 0;
-
-bool checkIOI(int idx) {
-	if (s[idx + 1] == 'O' && s[idx + 2] == 'I') {
-		return true;
-	}
-	return false;
-}
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-
-	cin >> n >> m;
-	cin >> s;
-
-	for (int i = 0; i < m - 2; i++) {
+	cin >> n >> m >> s;
+	for (int i = 0; i < s.length(); i++)
+	{
+		int countt = 0;
 		if (s[i] == 'I') {
-			int partLen = 0;
-			while (true) {
-				if ((i < m - 2) && checkIOI(i)) {
-					partLen++;
-					i += 2;
-					//cout << "partLen : " << partLen << endl;
-					//cout << "i : " << i << endl;
-				}
-				else {
-					if (partLen >= n) answer += partLen - n + 1;
-					//answer += partLen - n + 1;
-					break;
+			while (s[i + 1] == 'O' && s[i + 2] == 'I') {
+				i += 2;
+				countt++;
+				if (countt == n) {
+					cntt++;
+					countt--;
 				}
 			}
+			countt = 0;
 		}
 	}
-
-	cout << answer;
-
-	return 0;
+	cout<<cntt<<endl;
 }
