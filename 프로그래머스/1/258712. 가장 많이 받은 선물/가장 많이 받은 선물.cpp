95c6e27cbcb066531b2da5b7c4d45af5c6e2592b
tintin010 <82323029+tintin010@users.noreply.github.com>
2025-02-13 15:06:04 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 8.40 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -10,6 +10,7 @@ int solution(vector<string> friends, vector<string> gifts) {
     int answer = 0;
     int len = friends.size();
     map<string, int> person;
+    vector<int> index(len, 0);
     int gift_arr[len][len];
     
     memset(gift_arr, 0, sizeof(gift_arr));
@@ -26,6 +27,9 @@ int solution(vector<string> friends, vector<string> gifts) {
         ss >> give >> take;
         
         gift_arr[person[give]][person[take]] += 1;
+        
+        index[person[give]]++;
+        index[person[take]]--;
     }
     
     int nextmonth[len];
@@ -33,27 +37,27 @@ int solution(vector<string> friends, vector<string> gifts) {
     
     for(int i = 0; i < len; i++){
         for(int j = 0; j < len; j++){
-            if(i == j) continue;
+            if(i == j || gift_arr[i][j] < gift_arr[j][i]) continue;
             if(gift_arr[i][j] > gift_arr[j][i]){
                 nextmonth[i]++;
-            }else if(gift_arr[i][j] < gift_arr[j][i]){
-                nextmonth[j]++;
-            }else{
-                int tempI = 0;
-                int tempJ = 0;
-                for(int k = 0; k < len; k++){
-                    tempI = tempI + gift_arr[i][k] - gift_arr[k][i];
-                    tempJ = tempJ + gift_arr[j][k] - gift_arr[k][j];
-                }
-                if(tempI > tempJ) nextmonth[i]++;
-                else if(tempJ > tempI) nextmonth[j]++;
-                else continue;
+            }else if(index[i] > index[j]){
+                nextmonth[i]++;
+                // int tempI = 0;
+                // int tempJ = 0;
+                // for(int k = 0; k < len; k++){
+                //     tempI = tempI + gift_arr[i][k] - gift_arr[k][i];
+                //     tempJ = tempJ + gift_arr[j][k] - gift_arr[k][j];
+                // }
+                // if(tempI > tempJ) nextmonth[i]++;
+                // else if(tempJ > tempI) nextmonth[j]++;
+                // else continue;
             }
         }
     }
     for(int i = 0; i < len; i++){
         if(answer < nextmonth[i]) answer = nextmonth[i];
     }
+    // answer /=2;
     
-    return answer/2;
+    return answer;
 }
