e34e0bc604f73d629e5a76bab108022a5795b954
tintin010 <82323029+tintin010@users.noreply.github.com>
2025-02-14 18:49:30 +0900

    [level 2] Title: 문자열 압축, Time: 1.19 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -9,6 +9,7 @@ int solution(string s) {
     int answer = 1000;
     
     string str;
+    int count;
     
     if (s.length() == 1) {
         return 1;
@@ -17,8 +18,8 @@ int solution(string s) {
     for(int i = 1; i <= s.length()/2; i++){
         string temp = "";
         str = s.substr(0, i);
-        int count = 1;
-        for(int j = i; j <= s.length(); j += i){
+        count = 1;
+        for(int j = i; j < s.length(); j += i){
             if(str.compare(s.substr(j, i)) == 0){
                  count++;
             }else{
@@ -32,6 +33,7 @@ int solution(string s) {
                 count = 1;
             }
         }
+        if(count > 1) temp += to_string(count);
         temp += str;
         if(answer > temp.length()){
              answer = temp.length();
