e98d3e2e9de0525b0c28314e03d80a2069af1959
김재윤 <82323029+tintin010@users.noreply.github.com>
2025-06-13 13:09:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,39 +1,32 @@
-//1654 랜선 자르기
-
 #include <iostream>
-#include <algorithm>
-#include <math.h>
 
 using namespace std;
 
-int n, cn;
-long long len[10001];
-long long answer = 0;
+int main(){
+    long long tree[10001];
+    int k, n;
+    int result = 0;
+    cin >> k >> n;
+    for(int i = 0; i < k; i++) tree[i] = 0;
+    long long min = 1, max = 0;
 
+    for(int i = 0; i < k; i++){
+        long long l;
+        cin >> l;
+        tree[i] = l;
+        if(max < l) max = l;
+    }
 
-int main() {
-	cin >> n >> cn;
-	long long m1 = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> len[i];
-		m1 = max(m1, len[i]);
-	}
-	long long lp = 1, hp = m1;
-	while (lp <= hp) {
-		long long por = 0;
-		long long mid = (lp + hp) / 2;
-		for (int i = 0; i < n; i++) {
-			por += len[i] / mid;
-		}
-		if (por >= cn) {
-			lp = mid + 1;
-			answer = max(answer, mid);
-		}
-		else {
-			hp = mid - 1;
-		}
-	}
-
-	cout << answer << endl;
-
+    while(min <= max){
+        long long mid = (min + max) / 2;
+        int temp = 0;
+        for(int i = 0; i < k; i++) temp += tree[i] / mid;
+        if(temp >= n){
+            min = mid + 1;
+            result = mid;
+        }else{
+            max = mid - 1;
+        }
+    }
+    cout << result << '\n';
 }
