3e48c2b6b77ef104388c474b1fb9d98b6d6e0ffa
김재윤 <82323029+tintin010@users.noreply.github.com>
2025-07-09 14:06:36 +0900

    [Silver II] Title: 종이의 개수, Time: 996 ms, Memory: 37176 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[3000][3000];
+int n;
+int ans[3];
+
+void cut(int x, int y, int a){
+    
+    if(a == 1){
+        int t = arr[x][y];
+        if(t == -1) ans[0]++;
+        else if(t == 0) ans[1]++;
+        else if(t == 1) ans[2]++;
+        return;
+    }
+
+    int t = arr[x][y];
+    int flag = 1;
+    
+    for(int i = x; i < x + a; i++){
+        for(int j = y; j < y + a; j++){
+            if(t != arr[i][j]){
+                flag = 0;
+            }
+            if(flag == 0){
+                int d = a / 3;
+
+                cut(x, y, d);
+                cut(x, y + d, d);
+                cut(x + d, y, d);
+                cut(x + d, y + d, d);
+                cut(x + d, y + 2 * d, d);
+                cut(x + 2 * d, y + d, d);
+                cut(x + 2 * d, y, d);
+                cut(x, y + 2 * d, d);
+                cut(x + 2 * d, y + 2 * d, d);
+                return;
+            }
+        }
+    }
+
+    if(t == -1) ans[0]++;
+    else if(t == 0) ans[1]++;
+    else if(t == 1) ans[2]++;
+
+    return;
+}
+
+int main(){
+
+    for(int i = 0; i < 3; i++) ans[i] = 0;
+    
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            int k;
+            cin >> k;
+            arr[i][j] = k;   
+        }
+    }
+
+    cut(0, 0, n);
+
+    for(int i = 0; i < 3; i++)
+        cout << ans[i] << "\n";
+    
+    return 0;
+
+}
